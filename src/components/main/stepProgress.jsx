88be3bc8c36cea331a5dedb03d48368599be8c0f
Tim0124 <a0925172969@gmail.com>
2023-05-03 17:40:29 +0800

    Modified the stepProgress ＆ navbar menu css style

--- src/components/main/stepProgress.jsx
+++ src/components/main/stepProgress.jsx
@@ -1,22 +1,22 @@
-import './stepProgress.scss'
+import style from './stepProgress.module.scss'
 
 function StepProgress () {
   return (
-      <div className="stepperPanel">
-        <div className="stepperContainer">
-          <div className="step ">
-            <div className="circleContainer"></div>
-            <div className="labelContainer">寄送地址</div>
-            <span className='connectLine'></span>
+      <div className={style.stepperPanel}>
+        <div className={style.stepperContainer}>
+          <div className={style.step}>
+            <div className={`${style.circleContainer} ${style.circleNumOne}`}></div>
+            <div className={style.labelContainer}>寄送地址</div>
           </div>
-          <div className="step ">
-            <div className="circleContainer"></div>
-            <div className="labelContainer">運送方式</div>
-            <span className='connectLine'></span>
+          <span className={`${style.connectLine} ${style.lineOne}`}></span>
+          <div className={style.step}>
+            <div className={`${style.circleContainer} ${style.circleNumTwo}` }></div>
+            <div className={style.labelContainer}>運送方式</div> 
           </div>
-          <div className="step">
-            <div className="circleContainer"></div>
-            <div className="labelContainer">付款資訊</div>
+          <span className={`${style.connectLine} ${style.lineTwo}`}></span>
+          <div className={style.step}>
+            <div className={`${style.circleContainer} ${style.circleNumThree}`}></div>
+            <div className={style.labelContainer}>付款資訊</div>
           </div>
         </div>
       </div>      
